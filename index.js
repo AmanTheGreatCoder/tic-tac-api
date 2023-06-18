1200f05e8dc76118b294386fadffe7b57f44e191
AmanTheGreatCoder <amanwadhwaniya159@gmail.com>
2023-06-18 17:52:43 +0530

    Jun 18 Commit!

--- index.js
+++ index.js
@@ -8,37 +8,47 @@ const wss = new WebSocket.Server({ server });
 
 const clients = {};
 const games = {};
+// const initialBoard = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+const initialBoard = [2, 1, 0, 2, 1, 0, 0, 1, 0];
+// [1, 2, 2, 2, 1, 1, 2, 1, 2]; Draw scenario
 
 wss.on('connection', (connection) => {
   console.log('Client Connected');
   const id = v4();
   clients[id] = connection;
-  let payload = {
-    method: 'connect',
-    clientId: id,
-  };
-  connection.send(JSON.stringify(payload));
+  connection.send(
+    JSON.stringify({
+      method: 'connect',
+      clientId: id,
+    })
+  );
 
   connection.on('open', () => {
     console.log('connection open');
   });
 
-  connection.on('close', () => {
+  connection.on('close', (res) => {
     console.log('connection closed');
   });
 
   connection.on('message', (res) => {
     try {
       const data = JSON.parse(res?.toString('utf-8'));
-      let clientId = data?.clientId;
-      let gameId = data?.gameId;
+      let clientId;
+      let gameId;
       console.log('Message Recieved', data);
       if (data?.method === 'create') {
         clientId = data.clientId;
-        gameId = v4().substring(0, 4);
+        // gameId = v4().substring(0, 4);
+        gameId = '1382';
         games[gameId] = {
           id: gameId,
           clientIds: [clientId],
+          state: {
+            board: initialBoard,
+            turn: null,
+            gameOver: false,
+          },
         };
         const payload = {
           method: 'create',
@@ -49,31 +59,123 @@ wss.on('connection', (connection) => {
       }
 
       if (data?.method === 'join') {
-        const con = clients[clientId];
+        clientId = data.clientId;
+        gameId = data.gameId;
+
         if (games[gameId]) {
-          games[gameId].clientIds.push(clientId);
-          con.send(JSON.stringify(games[gameId]));
+          if (!games[gameId].clientIds.includes(clientId)) {
+            if (games[gameId].clientIds.length === 2) {
+              sendError(
+                'Sorry you can not join the game. Max Player Limit Reached',
+                clientId
+              );
+              return;
+            }
+            games[gameId].clientIds.push(clientId);
+            games[gameId].state.turn =
+              games[gameId].clientIds[Math.round(Math.random())];
+          }
+          const payload = {
+            method: 'join',
+            game: games[gameId],
+          };
+          console.log('sending payload in join method ', payload);
+          sendDataToClients(gameId, payload);
         } else {
-          // game Id does not exits
+          sendError(`There is no game with id: ${gameId}`, clientId);
         }
       }
 
       if (data.method === 'gameData') {
+        clientId = data.clientId;
+        gameId = data.gameId;
+        sendGameData(gameId);
+      }
+
+      if (data.method === 'close') {
+        gameId = data.gameId;
+        clientId = data.clientId;
+        delete clients[clientId];
+        const index = games[gameId].clientIds.indexOf(clientId);
+        if (index > -1) {
+          games[gameId].clientIds.splice(index, 1);
+          sendGameData(gameId);
+        }
+      }
+
+      if (data.method === 'gameOver') {
+        gameId = data.gameId;
+        const game = games[gameId];
+        game.state.gameOver = true;
         const payload = {
-          method: 'gameData',
+          method: 'gameOver',
           game: games[gameId],
         };
-        const con = clients[clientId];
-        con.send(JSON.stringify(payload));
+        sendDataToClients(gameId, payload);
+      }
+
+      if (data.method === 'reset') {
+        gameId = data.gameId;
+        games[gameId].state.board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+        games[gameId].state.gameOver = false;
+        sendGameData(gameId);
+        sendDataToClients(gameId, { method: 'reset' });
       }
 
       if (data.method === 'tileClick') {
+        gameId = data.gameId;
+        clientId = data.clientId;
+        const index = data.index;
+        const game = games[gameId];
+        const clIndex = game.clientIds.indexOf(clientId);
+        if (game.state.board[index] === 0) {
+          if (clIndex === 0) {
+            game.state.board[index] = 1;
+          } else if (clIndex === 1) {
+            game.state.board[index] = 2;
+          }
+
+          if (game.state.turn === game.clientIds[0]) {
+            game.state.turn = game.clientIds[1];
+          } else {
+            game.state.turn = game.clientIds[0];
+          }
+        }
+        sendGameData(gameId);
       }
     } catch (err) {
       console.log('error in message method', err);
     }
   });
 });
+
+function sendGameData(gameId) {
+  const payload = {
+    method: 'gameData',
+    game: games[gameId],
+  };
+
+  sendDataToClients(gameId, payload);
+}
+
+function sendDataToClients(gameId, payload) {
+  const connections = games[gameId].clientIds;
+  for (let i = 0; i < connections.length; i++) {
+    console.log('sending data to client ', connections[i]);
+    clients[connections[i]].send(JSON.stringify(payload));
+  }
+}
+
+function sendError(msg, clientId) {
+  console.log('sending error');
+  const payload = {
+    method: 'error',
+    message: msg,
+  };
+
+  const client = clients[clientId];
+  client.send(JSON.stringify(payload));
+}
 
 server.listen(8080, () => {
   console.log('Server Listening on 8080');
